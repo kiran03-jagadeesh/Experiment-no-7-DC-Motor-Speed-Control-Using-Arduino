a02651a4d480e2e87f9c719676dbd688c2f23e28
kiran <94174536+kiran03-jagadeesh@users.noreply.github.com>
2022-05-20 18:22:41 +0530

    Add files via upload

---
+++ _ex_no_06_dc_motor1.ino
@@ -0,0 +1,16 @@
+#define m1 2
+#define m2 3
+
+void setup()
+{
+  pinMode(m1, OUTPUT);
+  pinMode(m2, OUTPUT);
+}
+
+	void loop()
+{
+	digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+      delay(10000);
+    }
+      
